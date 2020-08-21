4d446f5f664ca567c70a4bad87b122f98b9216d6
Kevin Ponciano <69651491+Sabrine-CS@users.noreply.github.com>
2020-08-21 18:50:47 -0300

    Site test

---
+++ script.js
@@ -0,0 +1,51 @@
+function verificar() {
+  var data = new Date() //data recebe data atual do pc
+  var ano = data.getFullYear()// ano recebe ano atual 
+  var fano = document.getElementById('txtano')
+  var res = document.querySelector('div#res')
+  if (fano.value.length == 0 || Number (fano.value) > ano) {
+      window.alert('[ERRO] Verifique os dados e tente novamente!')
+  } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var gênerp = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+        gênero = 'Homem'
+        if (idade >= 0 && idade <10) { // se a idade for entre 0 e 10 é um bebe
+            //bebe
+            img.setAttribute('src', 'bebe-m.png')
+        } else if (idade <21) {
+                //jovem
+                img.setAttribute('src', 'jovem-m.png')
+            }else if (idade <50) {
+                //adulto
+                img.setAttribute('src', 'adulto-m.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'idoso-m.png')
+            }
+        
+    } else if (fsex[1].checked){
+        gênero = 'Mulher'
+        if (idade >= 0 && idade <10) { // se a idade for entre 0 e 10 é um bebe
+            //bebe
+            img.setAttribute('src', 'bebe-f.png')
+        } else if (idade <21) {
+                //jovem
+                img.setAttribute('src', 'jovem-f.png')
+            }else if (idade <50) {
+                //adulto
+                img.setAttribute('src', 'adulto-f.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'idoso-f.png')
+            }
+    }
+    res.style.textAlign = 'center' // res fica centralizado
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos. `
+    res.appendChild(img)
+  }
+  
+}
